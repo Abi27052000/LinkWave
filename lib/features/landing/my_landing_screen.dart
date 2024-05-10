8170d650ccc8de0c7194b41840d08f113349cc9d
Alles Abinash <allesabinash1@gmail.com>
2024-05-10 07:42:20 +0530

    Add final landing screen

--- lib/features/landing/my_landing_screen.dart
+++ lib/features/landing/my_landing_screen.dart
@@ -6,38 +6,29 @@ class MyLandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-          children: [Container(
-            
-            decoration: BoxDecoration(
+      body: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
               image: DecorationImage(
-                fit:BoxFit.cover ,
-                image: AssetImage('assets/mm.jpg'))
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            child: ElevatedButton(onPressed: () {}, 
-            child:Text('AGREE AND CONTINUE',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-            ),
-            ),
-            
-            style:ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              minimumSize: Size(MediaQuery.of(context).size.width, 70),
-              
-            )
-              
-                      
-                        
-            ),
-          )
-          ]
-          ),
+                  fit: BoxFit.cover, image: AssetImage('assets/mm.jpg'))),
+        ),
+        Positioned(
+          bottom: 20,
+          child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'AGREE AND CONTINUE',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: Size(MediaQuery.of(context).size.width, 70),
+              )),
+        )
+      ]),
     );
   }
 }
